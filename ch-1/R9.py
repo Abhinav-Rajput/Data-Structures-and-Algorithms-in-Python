0236fdc0b1f41874e8182cdf18c358996d146854
IN\abhinav.singh <AbhinavRajput2011@gmail.com>
2017-06-13 18:21:24 +0530

    range with values 50, 60, 70, 80

---
+++ ch-1/R9.py
@@ -0,0 +1,6 @@
+# R1.9 What parameters should be sent to the range constructor, to produce a
+# range with values 50, 60, 70, 80?
+
+if __name__=='__main__':
+    for i in range(50,90,10):
+        print(i)
