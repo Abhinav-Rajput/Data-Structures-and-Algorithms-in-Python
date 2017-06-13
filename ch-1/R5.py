026f62dca95453d7b0abd7bf2fcdc373c90e071f
IN\abhinav.singh <AbhinavRajput2011@gmail.com>
2017-06-13 16:29:55 +0530

    comprehension syntax and the built-in sum function

--- ch-1/R5.py
+++ ch-1/R5.py
@@ -1,2 +1,12 @@
 # R 1.5 Give a single command that computes the sum from Exercise R-1.4, relying
 # on Python’s comprehension syntax and the built-in sum function.
+
+
+def sum_List(n):
+    return sum([a * a for a in range(n)])
+
+if __name__=='__main__':
+    print(sum_List(n))
+
+
+sum_List(10)    
