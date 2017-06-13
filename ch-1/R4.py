21f75ee316f6ea74a70e2d45f04ca1893d9b4117
IN\abhinav.singh <AbhinavRajput2011@gmail.com>
2017-06-13 16:21:11 +0530

    sum of the squares of all the positive integers smaller than n

--- ch-1/R4.py
+++ ch-1/R4.py
@@ -1,7 +1,6 @@
 # R-1.4 Write a short Python function that takes a positive integer n and returns
 # the sum of the squares of all the positive integers smaller than n.
 
-
 def sum_of_squares(n):
     sum = 0
     for i in range(1, n):
